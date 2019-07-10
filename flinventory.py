536f0b1c51abd499444024c7aa5f056415271812
zgf2022 <zgf2022@gmail.com>
2019-07-09 21:37:24 -0500

    add template

--- flinventory.py
+++ flinventory.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('layout.html')
+	
+if __name__ == '__main__':
+    app.run(debug=True)
